70acd3b0a4d7beb76dc88961fa8d68d2fa0c5dc8
Peter Amir <Peteramir5522@gmail.com>
2022-07-16 21:44:12 +0200

    feat: Split books-lists page into components

---
+++ src/components/BookShelf.js
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+import Book from "./Books";
+
+const Bookshelf = ({ type, books }) => {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">
+        {type === "currentlyReading" && "Currently Reading"}
+        {type === "wantToRead" && "Want to Read"}
+        {type === "read" && "Read"}
+      </h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books
+            .filter((book) => book.shelf === type)
+            .map((book) => {
+              return (
+                <li>
+                  <Book book={book} />
+                </li>
+              );
+            })}
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default Bookshelf;
+
+Bookshelf.propType = {
+  type: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired,
+};
