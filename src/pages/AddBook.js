819dd37b1897e140e4d6ae2a0cf3784407a5e484
Peter Amir <Peteramir5522@gmail.com>
2022-07-16 23:57:44 +0200

    feat: Implement search functionality

--- src/pages/AddBook.js
+++ src/pages/AddBook.js
@@ -1,6 +1,33 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Book from "../components/Books";
+import * as BooksApi from "../BooksAPI";
 
 const AddBook = () => {
+  const [searchValue, setSearch] = useState("");
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const res = await BooksApi.search(searchValue, undefined);
+      if (res?.items) {
+        setBooks([]);
+      } else {
+        setBooks(res);
+      }
+    };
+
+    if (searchValue === "") {
+      return;
+    }
+    search();
+  }, [searchValue]);
+
+  const updateSearchInput = (e) => {
+    const value = e.target.value;
+    setSearch(value);
+  };
+
   return (
     <div className="search-books">
       <div className="search-books-bar">
@@ -8,11 +35,20 @@ const AddBook = () => {
           Close
         </Link>
         <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title, author, or ISBN" />
+          <input
+            type="text"
+            value={searchValue}
+            onChange={(e) => updateSearchInput(e)}
+            placeholder="Search by title, author, or ISBN"
+          />
         </div>
       </div>
       <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <ol className="books-grid">
+          {books.map((book) => {
+            return <Book key={book.id} book={book} />;
+          })}
+        </ol>
       </div>
     </div>
   );
